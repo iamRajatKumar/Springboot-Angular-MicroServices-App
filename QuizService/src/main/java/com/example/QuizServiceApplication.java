73b0bbf26c19daf53d2c5d9f2683df11f6bee47b
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-08-31 20:59:41 +0530

    packages and classes added

--- QuizService/src/main/java/com/example/QuizServiceApplication.java
+++ QuizService/src/main/java/com/example/QuizServiceApplication.java
@@ -1,9 +1,11 @@
-package com.example.QuizService;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class QuizServiceApplication {
 
 	public static void main(String[] args) {
