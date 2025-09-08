3cfe7748c356bc102ef6f76e89831e1bd0b99920
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-09-08 16:03:11 +0530

    required components added

---
+++ quiz-question-app-frontend/src/app/components/question/question-list/question-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-list',
+  standalone: false,
+  templateUrl: './question-list.component.html',
+  styleUrl: './question-list.component.css'
+})
+export class QuestionListComponent {
+
+}
