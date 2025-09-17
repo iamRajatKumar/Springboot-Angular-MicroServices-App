aef904c9e937eaf0144458bfdd4663f206b5fd18
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-09-17 21:52:00 +0530

    login code completed

--- quiz-question-app-frontend/src/app/components/auth/login/login.component.ts
+++ quiz-question-app-frontend/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login({ username: this.username, password: this.password }).subscribe({
+      next: (res) => {
+        this.authService.saveToken(res.token);
+        this.router.navigate(['/quiz']); // redirect after login
+      },
+      error: () => {
+        this.errorMessage = 'Invalid username or password';
+      }
+    });
+  }
 }
