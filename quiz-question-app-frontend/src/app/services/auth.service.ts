ba1ceedce834280488d8723e522226638e4c40a6
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-09-17 20:09:35 +0530

    auth service code completed

--- quiz-question-app-frontend/src/app/services/auth.service.ts
+++ quiz-question-app-frontend/src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8083/auth'; // API Gateway route
+
+  constructor(private http: HttpClient) {}
+
+  login(user: User): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, user);
+  }
+
+  signup(user: User): Observable<any> {
+    return this.http.post(`${this.baseUrl}/signup`, user);
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
 }
