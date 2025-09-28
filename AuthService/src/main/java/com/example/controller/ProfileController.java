242734264f8fb09087ff01997676ff9f384c88c7
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-09-28 22:40:53 +0530

    minor changes

--- AuthService/src/main/java/com/example/controller/ProfileController.java
+++ AuthService/src/main/java/com/example/controller/ProfileController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/profile")
+@RequestMapping("/profile")
 public class ProfileController {
 
     @Autowired
