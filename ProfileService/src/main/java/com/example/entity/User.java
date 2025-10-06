1e559158cf349b95b73c64eeb13ddfa19b4e3b02
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-10-06 18:31:40 +0530

    Added folders, files and codes

---
+++ ProfileService/src/main/java/com/example/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;     // used as login identifier
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String description;
+
+    private String profilePictureUrl; // stores file URL/path
+
+}
